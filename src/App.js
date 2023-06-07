650be84aae01b7257bc85ce1cd45c76799c67590
martincardarilli <martin.cardarilli@gmail.com>
2023-06-07 18:48:46 -0300

    intentando arreglar la navegacion y el style del proxy que no se carga

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ import { getRoutes } from 'routing/helper';
 import routesAndMenuItems from 'routes.js';
 import Loading from 'components/loading/Loading';
 
+import { BrowserRouter } from 'react-router-dom';
+
 const App = () => {
   const { currentUser, isLogin } = useSelector((state) => state.auth);
 
   const routes = useMemo(() => getRoutes({ data: routesAndMenuItems, isLogin, userRole: currentUser.role }), [isLogin, currentUser]);
   if (routes) {
     return (
+      <BrowserRouter basename="/criptocommerce">
       <Layout>
         <RouteIdentifier routes={routes} fallback={<Loading />} />
       </Layout>
+      </BrowserRouter>
     );
   }
   return <></>;
